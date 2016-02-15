357f38f18d8a9384cef51c65578098867f6e6b0f
Fred6681 <frederic.cesart@yahoo.fr>
2016-02-16 00:38:58 +0100

    Add Darkness governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -424,6 +424,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLUACTIVE)
 extern struct cpufreq_governor cpufreq_gov_bluactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bluactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 
